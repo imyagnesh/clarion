9a09fc05f6c80d92fe289af32d4312d985908815
Yagnesh Modh <yagnesh.modh@gmail.com>
2021-11-09 11:46:42 +0530

    completed data type

---
+++ dataTypes.js
@@ -0,0 +1,20 @@
+// Data Types
+
+// Primitive
+
+// 1. string
+// 2. number
+// 3. boolean
+// 4. symbol
+// 5. bigint
+
+//  Non Primitive
+
+// 1. Objects
+// 2. Array
+// 3. Null
+// 4. Undefined
+
+let a = true;
+
+console.log(typeof a);
