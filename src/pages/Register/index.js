d21b918368d9c2e2c392f308929a6606be7daab2
Pramodvh <Pramod.Halapeti@ness.com>
2021-11-12 17:59:26 +0530

    completed registration form

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { LocaleContext } from '../../context/localeContext';
+import { registerationFields, registerInitialValues } from './registerationFields';
+import Form from '../../components/Form';
 
-const Register = () => (
-  <div>
-    <h1>Register</h1>
-    <LocaleContext.Consumer>
-      {data => <div>{data.locale}</div>}
-    </LocaleContext.Consumer>
-  </div>
-);
+
+const Register = () => {
+ 
+  return (
+    <>
+      <h1>Login</h1>
+      <Form
+        fields={registerationFields}
+        initialValues={registerInitialValues}
+        btnText="Login"
+        onSubmit={val => {
+          console.log(val);
+        }}
+      />
+    </>
+  );
+};
 
 export default Register;
