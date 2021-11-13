0aa180297a4257a5ad8595dbe959b06d7d4b1cb6
Pramodvh <Pramod.Halapeti@ness.com>
2021-11-13 16:06:01 +0530

    conflicts fixed

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,45 +1,22 @@
-import React, { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Typography from '@mui/material/Typography';
-import './register.css';
+import React from 'react';
+import { LocaleContext } from '../../context/localeContext';
+import { registerationFields, registerInitialValues } from './registerationFields';
 import Form from '../../components/Form';
-import { registerFields, registerInitialValues } from './registerFields';
-import { AuthContext } from '../../context/authContext';
-// import { LocaleContext } from '../../context/localeContext';
+
 
 const Register = () => {
-  const navigate = useNavigate();
-  const { register } = useContext(AuthContext);
-  //   const { locale, setLocale } = useContext(LocaleContext);
-
-  console.log('hello');
-
+ 
   return (
     <>
-      <h1>Register</h1>
+      <h1>Login</h1>
       <Form
-        validate={values => {
-          const errors = {};
-          if (values.password !== values.confirmPassword) {
-            errors.confirmPassword = 'confirm password should match password';
-          }
-          return errors;
-        }}
-        fields={registerFields}
+        fields={registerationFields}
         initialValues={registerInitialValues}
-        btnText="Register"
-        onSubmit={register}
+        btnText="Login"
+        onSubmit={val => {
+          console.log(val);
+        }}
       />
-      <Typography variant="body1" gutterBottom>
-        Go to Login{' '}
-        <Typography
-          onClick={() => navigate('/', { replace: true })}
-          variant="button"
-          component="span"
-        >
-          Login
-        </Typography>
-      </Typography>
     </>
   );
 };
---
+++ src/pages/Register/registerationFields.js
@@ -0,0 +1,56 @@
+import FormikInput from '../../components/FormikInput';
+
+export const registerationFields = [
+    {
+        name: 'name',
+        label: 'Name',
+        component: FormikInput,
+        validate: value => {
+          if (!value) {
+            return 'Required...';
+          }
+          return '';
+        },
+      },
+  {
+    name: 'email',
+    label: 'Email Address',
+    component: FormikInput,
+    validate: value => {
+      if (!value) {
+        return 'Required...';
+      }
+      return '';
+    },
+  },
+  {
+    name: 'password',
+    label: 'Password',
+    type: 'password',
+    component: FormikInput,
+    validate: value => {
+      if (!value) {
+        return 'Required...';
+      }
+      return '';
+    },
+  },
+  {
+    name: 'confirmpassword',
+    label: 'Confirm Password',
+    type: 'password',
+    component: FormikInput,
+    validate: value => {
+      if (!value) {
+        return 'Required...';
+      }
+      return '';
+    },
+  },
+];
+
+export const registerInitialValues = {
+  email: '',
+  password: '',
+  name: ''
+};
